82c897a04736c6e9b34dbc2946b5364fd3c71884
Nalzurin <alexnormandi@ukr.net>
2023-10-11 16:17:57 +0300

    Add task 2.7-2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,27 +1,48 @@
 import { useState } from "react";
 
-function Number({ name }) {
-  return <li>{name}</li>;
+function Number({ name, number }) {
+  return <li>{name} {number}</li>;
 }
 
 function PhoneBook({ persons }) {
+  console.log(persons);
   return persons.map((person) => {
     console.log("Logging person:", person);
-    return <Number key={person.name} name={person.name} />;
+    return <Number key={person.id} name={person.name} number={person.number} />;
   });
 }
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  var freeId = persons.length+1;
 
   const handleNameInputChange = (event) => {
     setNewName(event.target.value);
   };
-  const addNewPerson = (event) =>{
+  const handleNumberInputChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const addNewPerson = (event) => {
     event.preventDefault();
-    const object = {name: newName}; 
-    setPersons(persons.concat(object))
+    const object = { name: newName, number: newNumber, id: freeId};
+    const arrayCheck = persons.some(({ name }) => name === object.name);
+    console.log(`Phonebook contains the person ${arrayCheck}`);
+    if (arrayCheck) {
+      console.log(`Person not added`);
+      alert(`${object.name} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(object));
+      console.log(`Person added`);
+      freeId++;
+    }
+    setNewName("");
   };
 
   return (
@@ -33,7 +54,10 @@ function App() {
           name: <input value={newName} onChange={handleNameInputChange} />
         </div>
         <div>
-          <button type="submit" >add</button>
+          number: <input value={newNumber} onChange={handleNumberInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
