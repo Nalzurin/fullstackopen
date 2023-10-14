40e566ac1afcafa92840af166f1f57d25a20d920
Nalzurin <alexnormandi@ukr.net>
2023-10-14 17:17:37 +0300

    Add task 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Phonebook from "./Components/Phonebook";
 import InputForm from "./Components/InputForm";
 import Filter from "./Components/Filter";
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filterName, setFilterName] = useState("");
   const personsToShow = persons.filter((person) => {
     if (!filterName == "") {
@@ -16,7 +12,13 @@ function App() {
       return person.name.toLowerCase().includes(filterName.toLowerCase());
     } else return true;
   });
-
+  useEffect(() => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
