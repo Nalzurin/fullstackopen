8271d29a76664e859ff10d51e224de2bcf94fc38
Nalzurin <alexnormandi@ukr.net>
2023-11-16 15:41:23 +0200

    Add task 5.7 - 5.8

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+
+  const setVisibility = () =>{
+    setVisible(!visible);
+  }
+
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        <p>
+          {blog.title} by {blog.author}
+        </p>
+        <input type="button" onClick={setVisibility} value="Hide"></input>
+        <p>URL: {blog.url}</p>
+        <p>Likes: {blog.likes}</p>
+        <p>Added by: {blog.user.name}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div style={blogStyle}>
+        <p>
+          {blog.title} by {blog.author}
+        </p>
+        <input type="button" onClick={setVisibility} value="Show"></input>
+      </div>
+    );
+  }
+};
 
 export default Blog;
